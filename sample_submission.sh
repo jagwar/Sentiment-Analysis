01903bb09f392be4dcf9df9595d437ff82bc1985
Guillaume Salou <g.salou@gmail.com>
2021-10-19 08:27:13 +0200

    modify sample_submission for ovhai example

--- sample_submission.sh
+++ sample_submission.sh
@@ -3,4 +3,4 @@ ovhai job run \
 -v sentiment_fr_output:/app/data/output \
 -v sentiment_fr_run:/app/runs \
 --image gsalouovh/sentiment:main \
---gpu 4 --cpu 16 --mem 24 --name sentiment_fr_camembert_training
+-g 1
