4fb57c34f5c11c69356d68b2eda87a69d186814f
Guillaume Salou <g.salou@gmail.com>
2021-10-19 08:25:20 +0200

    modify sample_submission for ovhai

--- sample_submission.sh
+++ sample_submission.sh
@@ -1,6 +1,6 @@
-eai job submit \
---data $ORG_NAME.$ACCOUNT_NAME.sentiment_fr:/app/data/sentiment_sentences \
---data $ORG_NAME.$ACCOUNT_NAME.sentiment_fr_output:/app/data/output \
---data $ORG_NAME.$ACCOUNT_NAME.sentiment_fr_run:/app/runs \
---image registry.console.elementai.com/$ORG_NAME.$ACCOUNT_NAME/training-cam \
+ovhai job run \
+-v sentiment_fr:/app/data/sentiment_sentences \
+-v sentiment_fr_output:/app/data/output \
+-v sentiment_fr_run:/app/runs \
+--image gsalouovh/sentiment:main \
 --gpu 4 --cpu 16 --mem 24 --name sentiment_fr_camembert_training
